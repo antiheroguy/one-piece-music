90c61e212d0b27785b27a23cf871e0a4b2055a3d
Mr. Hero <averylongtips@gmail.com>
2022-06-30 20:51:15 +0700

    fix: add env

--- data.js
+++ data.js
@@ -1,23 +1,21 @@
-require('dotenv').config();
-const axios = require('axios');
+require('dotenv').config()
+const axios = require('axios')
 const model = require('./model');
 
 (async () => {
-    model.collection.drop();
+    model.collection.drop()
     for (let p = 1; p <= process.env.VUE_APP_PAGE; p++) {
-        let episodes = [];
-        let link = process.env.VUE_APP_DATA_SRC;
-        link += p > 1 ? '?page=' + p : '';
+        let link = process.env.VUE_APP_DATA_SRC
+        link += p > 1 ? '?page=' + p : ''
 
-        await axios({
-            url: link
-        }).then(res => {
-            episodes = res.data.episodes;
-        }).catch(err => {
-            console.log(`${p}: ${err}`);
-        });
+        try {
+            const { data: { episodes } } = await axios.get(link)
+            await model.insertMany(episodes)
+            console.log(`${p}: OK`)
+        } catch (err) {
+            console.log(`${p}: ${err}`)
+        }
 
-        await model.insertMany(episodes).then(() => console.log(`${p}: OK`));
     }
-    process.exit(0);
-})();
+    process.exit(0)
+})()
--- src/App.vue
+++ src/App.vue
@@ -1,233 +1,258 @@
 <template>
-    <div id="app">
-        <div class="container">
-            <div class="header">
-                <div class="form-group row">
-                    <div class="col-sm-7">
-                        <img src="./assets/logo.png">
-                    </div>
-                    <label class="col-sm-2 col-form-label">Episode</label>
-                    <div class="col-sm-3">
-                        <select class="form-control" @change="getData()" v-model="selected">
-                            <option v-for="item in episodes" :key="item._id" :value="item.episode" :selected="item.episode == selected">{{ item.episode }}</option>
-                        </select>
-                    </div>
-                </div>
-                <h1 v-if="title && release">{{ title }} ({{ release }})</h1>
-            </div>
-            <ul class="timeline">
-                <li v-for="item in stamps" :key="item._id" class="timeline-item">
-                    <div class="timeline-badge">{{ item.time }}</div>
-                    <div class="timeline-panel">
-                        <div class="timeline-heading">
-                            <h4 class="timeline-title">{{ item.song.titles.en || 'Unknown' }}</h4>
-                            <div class="timeline-panel-controls">
-                                <div class="controls">
-                                </div>
-                                <div class="timestamp" v-if="item.song.id">
-                                    <a href="javascript:;" class="text-muted" @click="play(item.song.id)">Play</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="timeline-body">Album: {{ item.album.titles.en || 'Unknown' }}</div>
-                    </div>
-                </li>
-            </ul>
+  <div id="app">
+    <div class="container">
+      <div class="header">
+        <div class="form-group row">
+          <div class="col-sm-7">
+            <img src="./assets/logo.png">
+          </div>
+          <label class="col-sm-2 col-form-label">Episode</label>
+          <div class="col-sm-3">
+            <select
+              class="form-control"
+              v-model="selected"
+            >
+              <option
+                v-for="item in episodes"
+                :key="item._id"
+                :value="item.episode"
+                :selected="item.episode == selected"
+              >{{ item.episode }}</option>
+            </select>
+          </div>
         </div>
-        <audio ref="audio"></audio>
+        <h1 v-if="title && release">{{ title }} ({{ release }})</h1>
+      </div>
+      <ul class="timeline">
+        <li
+          v-for="item in stamps"
+          :key="item._id"
+          class="timeline-item"
+        >
+          <div class="timeline-badge">{{ item.time }}</div>
+          <div class="timeline-panel">
+            <div class="timeline-heading">
+              <h4 class="timeline-title">{{ item.song.titles.en || 'Unknown' }}</h4>
+              <div class="timeline-panel-controls">
+                <div class="controls">
+                </div>
+                <div
+                  class="timestamp"
+                  v-if="item.song.id"
+                >
+                  <a
+                    href="javascript:;"
+                    class="text-muted"
+                    @click="play(item.song.id)"
+                  >Play</a>
+                </div>
+              </div>
+            </div>
+            <div class="timeline-body">Album: {{ item.album.titles.en || 'Unknown' }}</div>
+          </div>
+        </li>
+      </ul>
     </div>
+    <audio ref="audio"></audio>
+  </div>
 </template>
 <script>
 import axios from 'axios'
+
 export default {
-    data: () => ({
-        stamps: [],
-        episodes: [],
-        title: '',
-        release: '',
-        selected: null,
-        src: '',
-    }),
-    created() {
-        axios({
-            url: `/list`
-        }).then(res => {
-            this.episodes = res.data;
-            this.selected = res.data[0].episode;
-            this.getData();
-        }).catch(err => {
-            console.error(err);
-        });
-    },
-    methods: {
-        getData() {
-            axios({
-                url: `/ep/` + this.selected
-            }).then(res => {
-                this.stamps = res.data.stamps;
-                this.title = res.data.titles.en;
-                this.release = res.data.release;
-                document.title = `Episode ${this.selected}: ${this.title}`;
-            }).catch(err => {
-                console.error(err);
-            });
-        },
-        play(id) {
-            this.$refs['audio'].src = `${process.env.VUE_APP_AUDIO_SRC}/${id}.mp3`;
-            this.$refs['audio'].play();
-        },
+  data: () => ({
+    stamps: [],
+    episodes: [],
+    title: '',
+    release: '',
+    selected: null,
+    src: '',
+    apiEndpoint: process.env.API_ENDPOINT || '',
+  }),
+
+  mounted() {
+    this.getList()
+  },
+
+  watch: {
+    selected(episode) {
+      if (episode) {
+        this.getEpisode()
+      }
     }
+  },
+
+  methods: {
+    async getList() {
+      const { data } = await axios.get(`${this.apiEndpoint}/list`)
+      this.episodes = data
+      this.selected = data[0].episode
+    },
+
+    async getEpisode() {
+      const { data } = await axios.get(`${this.apiEndpoint}/ep/${this.selected}`)
+      this.stamps = data.stamps
+      this.title = data.titles.en
+      this.release = data.release
+      document.title = `Episode ${this.selected}: ${this.title}`
+    },
+
+    play(id) {
+      const { $refs: { audio } } = this
+      audio.src = `${process.env.VUE_APP_AUDIO_SRC}/${id}.mp3`
+      audio.play()
+    }
+  }
 }
 </script>
+
 <style lang="scss">
-@import 'https://fonts.googleapis.com/css?family=Libre+Franklin';
+@import "https://fonts.googleapis.com/css?family=Libre+Franklin";
 
 body {
-    font-family: 'Libre Franklin', sans-serif;
+  font-family: "Libre Franklin", sans-serif;
 
-    .header {
-        margin: 10px 0;
+  .header {
+    margin: 10px 0;
 
-        label {
-            text-align: right;
-        }
+    label {
+      text-align: right;
+    }
+  }
+
+  .timeline {
+    list-style: none;
+    padding: 20px 0 20px;
+    position: relative;
+
+    &:before {
+      background-color: #eee;
+      bottom: 0;
+      content: " ";
+      left: 50px;
+      margin-left: -1.5px;
+      position: absolute;
+      top: 0;
+      width: 3px;
     }
 
-    .timeline {
-        list-style: none;
-        padding: 20px 0 20px;
+    > li {
+      margin-bottom: 20px;
+      position: relative;
+
+      &:before,
+      &:after {
+        content: " ";
+        display: table;
+      }
+
+      &:after {
+        clear: both;
+      }
+
+      > .timeline-panel {
+        border-radius: 2px;
+        border: 1px solid #d4d4d4;
+        box-shadow: 0 1px 2px rgba(100, 100, 100, 0.2);
+        margin-left: 100px;
+        padding: 20px;
         position: relative;
 
-        &:before {
-            background-color: #eee;
-            bottom: 0;
-            content: " ";
-            left: 50px;
-            margin-left: -1.5px;
+        .timeline-heading {
+          .timeline-panel-controls {
             position: absolute;
-            top: 0;
-            width: 3px;
-        }
+            right: 8px;
+            top: 5px;
 
-        >li {
-            margin-bottom: 20px;
-            position: relative;
-
-            &:before,
-            &:after {
-                content: " ";
-                display: table;
+            .timestamp {
+              display: inline-block;
             }
 
-            &:after {
-                clear: both;
-            }
+            .controls {
+              display: inline-block;
+              padding-right: 5px;
+              border-right: 1px solid #aaa;
 
-            >.timeline-panel {
-                border-radius: 2px;
-                border: 1px solid #d4d4d4;
-                box-shadow: 0 1px 2px rgba(100, 100, 100, 0.2);
-                margin-left: 100px;
-                padding: 20px;
-                position: relative;
+              a {
+                color: #999;
+                font-size: 11px;
+                padding: 0 5px;
 
-                .timeline-heading {
-                    .timeline-panel-controls {
-                        position: absolute;
-                        right: 8px;
-                        top: 5px;
-
-                        .timestamp {
-                            display: inline-block;
-                        }
-
-                        .controls {
-                            display: inline-block;
-                            padding-right: 5px;
-                            border-right: 1px solid #aaa;
-
-                            a {
-                                color: #999;
-                                font-size: 11px;
-                                padding: 0 5px;
-
-                                &:hover {
-                                    color: #333;
-                                    text-decoration: none;
-                                    cursor: pointer;
-                                }
-                            }
-                        }
-
-                        .controls+.timestamp {
-                            padding-left: 5px;
-                        }
-                    }
+                &:hover {
+                  color: #333;
+                  text-decoration: none;
+                  cursor: pointer;
                 }
+              }
             }
 
-            .timeline-badge {
-                background-color: #999;
-                border-radius: 50%;
-                color: #fff;
-                font-size: 1em;
-                height: 50px;
-                left: 50px;
-                line-height: 52px;
-                margin-left: -25px;
-                position: absolute;
-                text-align: center;
-                top: 16px;
-                width: 50px;
-                z-index: 100;
+            .controls + .timestamp {
+              padding-left: 5px;
             }
+          }
+        }
+      }
 
-            .timeline-badge+.timeline-panel {
-                &:before {
-                    border-bottom: 15px solid transparent;
-                    border-left: 0 solid #ccc;
-                    border-right: 15px solid #ccc;
-                    border-top: 15px solid transparent;
-                    content: " ";
-                    display: inline-block;
-                    position: absolute;
-                    left: -15px;
-                    right: auto;
-                    top: 26px;
-                }
+      .timeline-badge {
+        background-color: #999;
+        border-radius: 50%;
+        color: #fff;
+        font-size: 1em;
+        height: 50px;
+        left: 50px;
+        line-height: 52px;
+        margin-left: -25px;
+        position: absolute;
+        text-align: center;
+        top: 16px;
+        width: 50px;
+        z-index: 100;
+      }
 
-                &:after {
-                    border-bottom: 14px solid transparent;
-                    border-left: 0 solid #fff;
-                    border-right: 14px solid #fff;
-                    border-top: 14px solid transparent;
-                    content: " ";
-                    display: inline-block;
-                    position: absolute;
-                    left: -14px;
-                    right: auto;
-                    top: 27px;
-                }
-            }
+      .timeline-badge + .timeline-panel {
+        &:before {
+          border-bottom: 15px solid transparent;
+          border-left: 0 solid #ccc;
+          border-right: 15px solid #ccc;
+          border-top: 15px solid transparent;
+          content: " ";
+          display: inline-block;
+          position: absolute;
+          left: -15px;
+          right: auto;
+          top: 26px;
         }
 
-        .timeline-title {
-            margin-top: 0;
-            color: inherit;
+        &:after {
+          border-bottom: 14px solid transparent;
+          border-left: 0 solid #fff;
+          border-right: 14px solid #fff;
+          border-top: 14px solid transparent;
+          content: " ";
+          display: inline-block;
+          position: absolute;
+          left: -14px;
+          right: auto;
+          top: 27px;
         }
-
-        .timeline-body {
-
-            >p,
-            >ul {
-                margin-bottom: 0;
-            }
-
-            >p+p {
-                margin-top: 5px;
-            }
-        }
+      }
     }
 
+    .timeline-title {
+      margin-top: 0;
+      color: inherit;
+    }
+
+    .timeline-body {
+      > p,
+      > ul {
+        margin-bottom: 0;
+      }
+
+      > p + p {
+        margin-top: 5px;
+      }
+    }
+  }
 }
 </style>
